d9eaea131f3152c974580a54c60296fcff2c5916
DenJan123 <dj421937@gmail.com>
2021-09-22 21:07:19 +0300

    stage 3 commit

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const addTaskButton = document.getElementById("add-task-button");
 const addTaskTextField = document.getElementById("input-task");
 const taskList = document.getElementById("task-list");
 let deleteButtons = document.querySelectorAll(".delete-btn");
+let checkBoxes = document.querySelectorAll("[type=checkbox]");
 let taskCount = 3;
 
 let template = (taskString) => `<li><input type="checkbox"><span class="task-${++taskCount} task">${taskString}</span><button class="delete-btn">Delete task</button></li>`;
@@ -9,21 +10,36 @@ let template = (taskString) => `<li><input type="checkbox"><span class="task-${+
 function getValueTextField() {
     return addTaskTextField.value;
 }
+
 function clearTextField() {
     addTaskTextField.value = '';
 }
+
 function appendTask() {
     let value = getValueTextField();
     if (!value) return;
     let entry = template(value);
     taskList.insertAdjacentHTML('beforeend', entry);
-    deleteButtons = document.querySelectorAll(".delete-btn");
-    deleteButtons.forEach(item => item.addEventListener("click", deleteRow))
+    updateEventListeners()
     clearTextField();
 }
 
 function deleteRow(e) {
     e.path[1].remove();
 }
-addTaskButton.addEventListener("click", appendTask)
-deleteButtons.forEach(item => item.addEventListener("click", deleteRow))
+function changeStyleTask(event)  {
+ //   event.currentTarget.nextElementSibling
+    event.currentTarget.nextElementSibling.classList.toggle("finished-task");
+    console.log('fired');
+}
+
+function updateEventListeners() {
+    let deleteButtons = document.querySelectorAll(".delete-btn");
+    let checkBoxes = document.querySelectorAll("[type=checkbox]");
+    deleteButtons.forEach(item => item.addEventListener("click", deleteRow));
+    checkBoxes.forEach(item => item.addEventListener('change', changeStyleTask));
+    addTaskButton.addEventListener("click", appendTask);
+
+}
+
+updateEventListeners()
